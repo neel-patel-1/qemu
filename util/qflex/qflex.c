ed345acfcc606f87c24e045f1fecf91a430a0daa
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2022-05-09 19:41:06 +0200

    fix: singlestep rerun instruction if failed first

--- util/qflex/qflex.c
+++ util/qflex/qflex.c
@@ -34,18 +34,23 @@ QflexState_t qflexState = {
     .log_inst = false
 };
 
-int qflex_singlestep(CPUState *cpu) {
-    int ret;
-    qflex_update_exit_main_loop(false);
-    if(cpu->halted) {
-        if (!cpu_has_work(cpu)) {
-            return 0;
-        }
-        cpu->halted = 0;
-    }
 
+
+static int qflex_singlestep_with_retry(CPUState *cpu, bool retry) {
+    int ret = 0;
+    qflex_update_exit_main_loop(false);
     uint64_t pc_ss = QFLEX_GET_ARCH(pc)(cpu);
     uint32_t asid = QFLEX_GET_ARCH(asid)(cpu);
+    uint64_t pc_ss_after;
+    // TODO: Fix when halted for multithreaded
+    // if(cpu->halted) {
+    //     qemu_log("CPU[%i]:ASID[%x]:HALTED", cpu->cpu_index, asid); 
+    //     if (!cpu_has_work(cpu)) {
+    //         return 0;
+    //     }
+    //     cpu->halted = 0;
+    // }
+
     if(qflexState.log_inst) {
         qemu_log("CPU[%i]:ASID[%x]:", cpu->cpu_index, asid); 
         QFLEX_GET_ARCH(log_inst)(cpu);
@@ -53,13 +58,29 @@ int qflex_singlestep(CPUState *cpu) {
     
     ret = qflex_cpu_step(cpu);
 
-    uint64_t pc_ss_after = QFLEX_GET_ARCH(pc)(cpu);
+    pc_ss_after = QFLEX_GET_ARCH(pc)(cpu);
     if(pc_ss == pc_ss_after) {
-        printf("QFlex singlestep went wrong: ret = %x\n", ret);
-        qemu_log("QFlex singlestep went wrong: ret = %x\n", ret);
+        if(retry) {
+            printf("QFlex singlestep went wrong twice in a row: ret = %x\n", ret);
+            qemu_log("QFlex singlestep went wrong twice in a row:: ret = %x\n", ret);
+            assert(false);
+        } else {
+            printf("QFlex singlestep went wrong, did not advance a step: ret = %x\n ---- Retrying\n", ret);
+            qemu_log("QFlex singlestep went wrong, did not advance a step: ret = %x\n ---- Retrying\n", ret);
+            ret = qflex_singlestep_with_retry(cpu, true);
+        }
+    }
+
+    if(retry) {
+        printf("QFlex singlestep retry passed\n");
+        qemu_log("QFlex singlestep went wrong\n");
     }
 
     return ret;
+}
+
+int qflex_singlestep(CPUState *cpu) {
+    return qflex_singlestep_with_retry(cpu, false);
 }
 
 int qflex_adaptative_execution(CPUState *cpu) {
